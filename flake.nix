e04acb838456243e896ab47168434f688bef1443
Andrew <liu.andrew@berkeley.edu>
2025-11-13 11:37:28 -0800

    init: fork data6 notes

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "CS61C Course Notes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let 
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ 
+		    quarto
+			python314
+			uv
+		];
+      };
+    });
+}
+
