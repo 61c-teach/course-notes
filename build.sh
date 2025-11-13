1ec885f58210caa50cae4c347eb87d411f8928ad
Andrew <liu.andrew@berkeley.edu>
2025-11-13 13:43:58 -0800

    fix: asdf install quarto correctly

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-asdf plugin add quarto
-asdf global quarto
+asdf plugin add quarto https://github.com/lucaswilric/asdf-quarto.git
+asdf install quarto latest
+asdf global quarto latest
 
 quarto render
