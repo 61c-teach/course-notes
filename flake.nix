312b6770374c54b72c57ca955fe92b40b117b9ef
Andrew <liu.andrew@berkeley.edu>
2025-11-20 15:54:00 -0800

    feat(nix): update build inputs

--- flake.nix
+++ flake.nix
@@ -3,22 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [ 
-		    quarto
-			python314
-			uv
-		];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            quarto
+            python314
+            uv
+          ];
+        };
+      }
+    );
 }
-
