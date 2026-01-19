75b0011b914919b4eb3c87d3ed86538c69767b8c
Andrew <liu.andrew@berkeley.edu>
2026-01-19 15:29:16 -0500

    feat(build): fix build config

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 # Install uv as python package manager
 asdf plugin add uv https://github.com/asdf-community/asdf-uv.git
 asdf install uv latest
+asdf global uv latest
 
 # Sync dependencies and install mystmd
 uv pip install mystmd
