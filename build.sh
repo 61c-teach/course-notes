7270317e075bce186641b656392814dc2c0b6a61
Lisa Yan <yanlisa@berkeley.edu>
2026-01-19 11:58:33 -0800

    feat(jupyter-book): fix cf build script
    asdf --> uv --> mystmd and book

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Install uv as python package manager
+asdf plugin add uv https://github.com/asdf-community/asdf-uv.git
+asdf install uv latest
+
 # Sync dependencies and install mystmd
 uv pip install mystmd
 
